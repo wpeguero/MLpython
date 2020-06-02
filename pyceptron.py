d3909f1ab859e337ca1c3de7de3d16289b0ec4d1
wpeguero <wpeguerorosario@outlook.com>
2020-06-02 16:57:06 -0400

    Finished the method plot_precision_regions.
    Finished creating the method, adapted it into the class.

--- pyceptron.py
+++ pyceptron.py
@@ -61,6 +61,22 @@ class Perceptron(object):
         """Return class label  after unit step."""
         return np.where(self.net_input(X) >= 0.0,1,-1)
 
-    def plot_precision_regions(X, y, classifier, resolution=0.02):
+    def plot_precision_regions(self, X, y, classifier, resolution=0.02):
         """Method utilized to visualize the decision boundaries for two dimensional datasets."""
-        pass
+        #Setup marker generator and color map
+        MARKERS = ('s', 'x', 'o', '^', 'v')
+        COLORS = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+        cmap = ListedColormap(COLORS[:len(np.unique(y))])
+        #plot the decision surface
+        x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+        x2_min, x2_max = X[:,1].min() - 1, X[:, 1].max() + 1
+        xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution))
+        Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+        Z = Z.reshape(xx1.shape)
+        plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
+        plt.xlim(xx1.min(), xx2.max())
+        plt.ylim(xx2.min(), xx2.max())
+
+        #plot class examples
+        for idx, cl in enumerate(np.unique(y)):
+            plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1], alpha=0.8, c=COLORS[idx], marker=MARKERS[idx], label=cl, edgecolor='black')
