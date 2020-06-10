ceedf8e9058987a48aa099a1af3b72e9f79fa39a
wpeguero <wpeguerorosario@outlook.com>
2020-06-10 19:59:04 -0400

    Modified the file. - Included two examples, one that showed the classification prowess of   the self-made logistic regression classifier and the other using the   logistic regression classifier created by scikit learn.

--- sklearn_tutorial.py
+++ sklearn_tutorial.py
@@ -1,7 +1,7 @@
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import Perceptron
+from sklearn.linear_model import Perceptron, LogisticRegression
 from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
 import numpy as np
@@ -63,7 +63,28 @@ def main():
     plt.ylabel('J(w)')
     plt.legend(loc='best')
     plt.tight_layout()
+    #Example of Logistic REgression model using Gradient Descent
+    X_train_01__subset = X_train[(y_train == 0) | (y_train == 1)]
+    y_train_01__subset = y_train[(y_train == 0) | (y_train == 1)]
+    lrgd = LogisticRegressionGD(eta=0.05, n=1000)
+    lrgd.fit(X_train_01__subset, y_train_01__subset)
+    plt.figure()
+    plot_decision_regions(X=X_train_01__subset, y=y_train_01__subset, classifier=lrgd)
+    plt.xlabel( 'petal length [standardized]' )
+    plt.ylabel( 'petal width [standardized]' )
+    plt.legend(loc= 'upper left' )
+    plt.tight_layout()
+    #Example of Logistic Regression Model using scikit Learning
+    lr = LogisticRegression(c=100.0, random_state=1, solver='lbfgs', multi_class='ovr')
+    lr.fit(X_train_std, y_train)
+    plt.figure()
+    plot_decision_regions(X_combined_std, y_combined, classifier=lr, test_idx=range(105, 150))
+    plt.xlabel( 'petal length [standardized]' )
+    plt.ylabel( 'petal width [standardized]' )
+    plt.legend( loc = 'upper left' )
+    plt.tight_layout()
     plt.show()
+    lr.predict_proba(X_test_std[:3, :])
 
 
 class LogisticRegressionGD(object):
