fb5834a6ec5e2e1e488cac8083e60526b76428db
wpeguero <wpeguerorosario@outlook.com>
2020-06-26 12:23:59 -0400

    Created Calculate_Humidity. Created a function that calculates the relative humidity based on the dry bulb temperature and the wet bulb temperature. The equation is not yet complete as there is still the need to convert the temperature to Celsius before doing any calculations.

--- weatherAI.py
+++ weatherAI.py
@@ -1,18 +1,25 @@
 import pandas as pd
 from datetime import datetime
+import numpy as np
 
 def main():
     """Trains data to predict the weather."""
     df__weather__raw = pd.read_csv(r'D:\Github\MLpython\Sample_Data\weather.csv', low_memory=False) 
     df__weather = remove_empty_columns(df__weather__raw, 0.95)
-    df__weather.to_csv("sample.csv")
+    relative_time = time_difference(df__weather)
+    relative_humidity = []
+    for row in df__weather.itertuples():
+        T_wet = row.HourlyWetBulbTemperature
+        T_dry = row.HourlyDryBulbTemperature
+        humidity = calculate_humidity(float(T_dry), float(T_wet))
+        relative_humidity.append(humidity)
+    df__weather['Time'] = relative_time
+    df__weather['RelativeHumidity'] = relative_humidity
+    df__weather.to_csv("sample__6_26_2020.csv")
 
 
 def test(): 
-    df__weather__raw = pd.read_csv(r'D:\Github\MLpython\Sample_Data\weather.csv', low_memory=False) 
-    df__weather = remove_empty_columns(df__weather__raw, 0.95)
-    time = []
-    time_difference(df__weather)
+    pass
 
 
 def remove_empty_columns(df, percentage):
@@ -33,10 +40,24 @@ def time_difference(df):
         dt.append(d)
         relative_time = []
     for i in range(len(dt)):
+
         if i > 0:
             t_relative = dt[i] - dt[i-1]
             relative_time.append(t_relative.total_seconds())
-    print(relative_time)
+        else:
+            t_relative = 0
+            relative_time.append(t_relative)
+    return relative_time
+
+def calculate_humidity(T__dry_bulb, T__wet_bulb):
+    """Calculates the relative humidity.
+    ------------------------------------
+    *Temperature is in 'Celcius*"""
+    N = 0.6687451584
+    e_dry = 6.112 * np.e ** ((17.502 * T__dry_bulb) / (240.97 *T__dry_bulb))
+    e_wet = 6.112 * np.e ** ((17.502 * T__wet_bulb) / (240.97 *T__wet_bulb))
+    relative_humidity = ((e_wet - N * (1 + .00115 * T__wet_bulb) * (T__dry_bulb - T__wet_bulb)) / e_dry) * 100
+    return relative_humidity
 
 
 if __name__ == "__main__":
