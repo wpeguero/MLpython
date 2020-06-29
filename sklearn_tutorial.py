a211dbd4a0fdbf3730af3cb53752de4f801ad1e9
wpeguero <wpeguerorosario@outlook.com>
2020-06-29 11:06:46 -0400

    Added the portion involving the kernel.

--- sklearn_tutorial.py
+++ sklearn_tutorial.py
@@ -124,6 +124,12 @@ def main():
     plt.ylim([-3, 3])
     plt.legend(loc='best')
     plt.tight_layout()
+    plt.figure()
+    svm = SVC(kernel='rbf', random_state=1, gamma=0.10, C=10.0)
+    svm.fit(X_xor, y_xor)
+    plot_decision_regions(X_xor, y_xor, svm)
+    plt.legend(loc='upper left')
+    plt.tight_layout()
     plt.show()
 
 
