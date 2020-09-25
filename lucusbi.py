4de49599a5e9dd25dc87621dcfd3b62346687aee
wpeguero <wpeguerorosario@outlook.com>
2020-09-24 22:02:43 -0400

    Removed use of two functions by labeling datatypes. Removed the use of the function utilized to extract groups from the dataframe as well as the function that takes the average of values based on grouping by assigning proper datatype to the data provided.

--- lucusbi.py
+++ lucusbi.py
@@ -6,20 +6,23 @@ def main():
     """Creates visuals for data that provide better insight as to what information the data can provide."""
     #%%Load the information
     df__estimated_inpatient = pd.read_csv(r'C:\Users\Benjamin\Documents\Programming\Github\MLpython\Sample_Data\estimated_inpatient_all_20200921_0928.csv')
-    groups__state = extract_groups(df__estimated_inpatient, 'state')
-    avg__IBOE__state__raw = avg_col_value(groups__state, 'Inpatient Beds Occupied Estimated')
-    print('Length of dictionary: ', len(avg__IBOE__state__raw))
-    x = []
-    y = []
-    avg__IBOE__state = {}
-    for key, avg in avg__IBOE__state__raw.items():
-        x.append(key)
-        y.append(avg)
-        avg__IBOE__state['{}'.format(key)] = avg
-    ax1 = sns.barplot(x,y)
-    df__estimated_inpatient['Inpatient Beds Occupied Estimated'] = df__estimated_inpatient['Inpatient Beds Occupied Estimated'].astype(float)
+    #%% Clean the Data
+    datatype = {
+        'state': 'category',
+        'Inpatient Beds Occupied Estimated': 'int',
+        'Count LL': 'int',
+        'Count UL': 'int',
+        'Percentage of Inpatient Beds Occupied Estimated': 'float',
+        'Percentage LL': 'float',
+        'Percentage UL': 'float',
+        'Total Inpatient Beds': 'int',
+        'Total LL': 'int',
+        'Total UL': 'int'
+    }
+    df__estimated_inpatient = df__estimated_inpatient.replace(',','',regex=True)
+    df__estimated_inpatient = df__estimated_inpatient.astype(datatype)
+    #%%Plot the Data
     ax2 = sns.barplot(x='state',y='Inpatient Beds Occupied Estimated', data=df__estimated_inpatient)
-    plt.show(ax1)
     plt.show(ax2)
 
 
@@ -45,7 +48,7 @@ def avg_col_value(dict_groups, col):
 
 def clean_data(df):
     """Converts numerical data into proper format, as well as remove any unwanted characters."""
-    pass
+    
 
 
 if __name__ == "__main__":
