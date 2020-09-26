f4edcdc74cc3aa21f17578bbab756a4b346d7ad7
Wilson B Peguero Rosario <bpeguero.wp@gmail.com>
2020-09-26 09:40:39 -0400

    Plot is now saved as a png. The script now finds the file path(generalizes the filepath for anyone to easily use and make the script abstract. Script now saves the plot to a .png file.

--- lucusbi.py
+++ lucusbi.py
@@ -1,11 +1,14 @@
-import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import os
 
 def main():
     """Creates visuals for data that provide better insight as to what information the data can provide."""
     #%%Load the information
-    df__estimated_inpatient = pd.read_csv(r'C:\Users\Benjamin\Documents\Programming\Github\MLpython\Sample_Data\estimated_inpatient_all_20200921_0928.csv')
+    path_to_file = os.path.abspath('Sample_Data/estimated_inpatient_all_20200921_0928.csv')
+    df__estimated_inpatient = pd.read_csv(path_to_file)
+    #/mnt/c/Users/Wpeguero/Documents/GitHub/MLpython/Sample_Data/estimated_inpatient_all_20200921_0928.csv
     #%% Clean the Data
     datatype = {
         'state': 'category',
@@ -22,8 +25,13 @@ def main():
     df__estimated_inpatient = df__estimated_inpatient.replace(',','',regex=True)
     df__estimated_inpatient = df__estimated_inpatient.astype(datatype)
     #%%Plot the Data
-    ax2 = sns.barplot(x='state',y='Inpatient Beds Occupied Estimated', data=df__estimated_inpatient)
-    plt.show(ax2)
+    plt.figure(figsize=(20,10))
+    ax1 = sns.barplot(x='state',y='Inpatient Beds Occupied Estimated', data=df__estimated_inpatient)
+    try:
+        plt.show(ax1)
+    except TypeError as e:
+        fig = ax1.get_figure()
+        fig.savefig('Estimation of Inpatiend Beds Occupied by State.png')
 
 
 def extract_groups(df, group_variable):
@@ -48,7 +56,7 @@ def avg_col_value(dict_groups, col):
 
 def clean_data(df):
     """Converts numerical data into proper format, as well as remove any unwanted characters."""
-    
+    pass
 
 
 if __name__ == "__main__":
