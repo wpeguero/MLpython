3794f76e8322c88a52c125cc2efd7524ec4c4455
wpeguero <wpeguerorosario@outlook.com>
2020-11-21 22:56:37 -0500

    Restarted the project from scratch. Decided to use diagnostic data in CSV files alone to create a program that is able to provide a diagnosis based on a description of the problem provided to it by a medical doctor.

--- DAI.py
+++ DAI.py
@@ -1,26 +1,27 @@
 """PROJECT: DAI
-Processes medical images and extracts regions of interest, converting the images into a value that can be used to estimate whether the ROI Diagnosis is a diseases or not.
+Diagnostic Artificial Intelligence, or DAI, is a program that will provide diagnoses based on the information provided for it, be it an image or an transcription.
+1. DescriptionDiagnosis - Provides a diagnosis based on the description provided by extracting keywords in the description and categorizing it under a medical specialty and sample name.
 
-Create a class called AIDiagnosis that will process the image and produce a diagnostic.
-- Base this class off of the NLP from spacy module."""
-from skimage import data, io, filters
-import matplotlib.pyplot as plt
+2. ImagingDiagnosis
+"""
 import pandas as pd
-import numpy as np
+import spacy as sp
 
 def main():
-    img = io.imread(r'C:\Users\Benjamin\Documents\Programming\Github\MLpython\Sample_Data\Medical_Imaging\CT_Scan\tiff_images\ID_0000_AGE_0060_CONTRAST_1_CT.tif')
-    print('img shape: ', img.shape)
-    signal = np.fft.fft2(img)
-    new_img = np.fft.ifft2(signal)
-    print('new img shape: ', new_img.shape)
-    plt.figure(1)
-    plt.imshow(img)
-    plt.figure(2)
-    plt.plot(signal)
-    #plt.figure(3)
-    #plt.imshow(new_img)
-    plt.show()
+    pass
+
+
+def load_diagnostic_data(df):
+    """
+    Labels all of the transcription field based on the keywords and the medical specialty.
+    """
+    pass
+
+def train_data(ldata):
+    """
+    Trains the loaded and parsed data into an nlp.
+    """
+    pass
 
 
 if __name__ == "__main__":
