9e7b7d1dc6363fb48636bee94dabb45dd68c2714
wpeguero <wpeguerorosario@outlook.com>
2020-11-27 14:49:15 -0500

    Finalized the ldd function and started simple alg. Created conditions that would raise an error in the case that the second and third variable are not string. Added a conditional statement that provides the function load_diagnostic_data the versatility to handle single label fields as well as multiple label fields with comma separated values.
    Started the hidden function codename LSD that will grab the labels
    created in the previous function and categorize it based on a specialty.

--- DAI.py
+++ DAI.py
@@ -13,37 +13,86 @@ def main():
     df__transcriptions = df__transcriptions.dropna(axis=0, how='any', subset=['transcription', 'keywords'])
     df__transcriptions.reset_index(drop=True)
     train_data = load_diagnostic_data(df__transcriptions, 'transcription','keywords')
-    with open(r'C:\Users\Benjamin\Documents\Programming\Github\MLpython\training_datav3.txt', 'w') as file:
-        text_train = str(train_data)
-        file.write(text_train)
-        file.close()
+    #Extract data into a sample file for reviewing
+    #with open(r'C:\Users\Benjamin\Documents\Programming\Github\MLpython\training_datav4.txt', 'w') as file:
+    #    text_train = str(train_data)
+    #    file.write(text_train)
+    #    file.close()
 
 
 def load_diagnostic_data(df,dcolumn,lcolumn):
     """
-    Labels all of the transcription field based on the keywords and the medical specialty.
+    Create Training Data from DataFrame
+    -----------------------------------
+    Function that extracts labels from a body of text based on the keywords provided on a separate field.
+    
+    Sample training data format:
+    
+    train_data = [
+    ("Uber blew through $1 million a week", [(0, 4, 'ORG')]),
+    ("Android Pay expands to Canada", [(0, 11, 'PRODUCT'), (23, 30, 'GPE')]),
+    ("Spotify steps up Asia expansion", [(0, 8, "ORG"), (17, 21, "LOC")]),
+    ("Google Maps launches location sharing", [(0, 11, "PRODUCT")]),
+    ("Google rebrands its business apps", [(0, 6, "ORG")]),
+    ("look what i found on google! 😂", [(21, 27, "PRODUCT")])]
     """
+    #Deal with bad inputs
+    if not isinstance(dcolumn, str):
+        raise TypeError(f'dcolumn must be a string, but is a {type(dcolumn)}')
+    if not isinstance(lcolumn, str):
+        raise TypeError(f'dcolumn must be a string, but is a {type(lcolumn)}')
+    #Initial conditions/formatting of the data
     traindata = []
     df[str(dcolumn)].astype(str)
     df[str(lcolumn)].astype(str)
     for index, row in df.iterrows():
         entities__list = []
-        kword__str = row[str(lcolumn)]
-        kword__list = kword__str.split(',')
-        for kword in kword__list:
-            transcription = row[str(dcolumn)]
-            transcription = transcription.lower()
-            if kword in transcription:
-                start = transcription.find(kword)
-                end = start + len(kword)
-                entity = (start, end, kword)
-                entities__list.append(entity)
-        entity__dict = {'entities': entities__list}
+        label__str = row[str(lcolumn)]
+        transcription = row[str(dcolumn)]
+        transcription = transcription.lower()
+        transcription = transcription.strip()
+        #Conditional statements for the fields provided
+        if ',' in label__str: #There are multiple labels in a field.
+            label__list = label__str.split(',') #Work on this portion to apply the load_diagnostic_data function in a general sense (i.e single label vs multiple labels)
+            for label in label__list:
+                label = label.lower()
+                label = label.strip()
+                if label in transcription:
+                    start = transcription.find(label)
+                    end = start + len(label)
+                    entity = (start, end, label)
+                    entities__list.append(entity)
+            entity__dict = {'entities': entities__list}
+        else: #There is only one label in the field.
+            label = label__str
+            label = label.lower()
+            label = label.strip()
+            if label in transcription:
+                start = transcription.find(label)
+                end = start + len(label)
+                entity = (start, end, label)
+            else:
+                start = 0
+                end = len(transcription)
+                entity = (start, end, label)
+            entity__dict = entity
         trainsample = (row[str(dcolumn)], entity__dict)
         traindata.append(trainsample)
     return traindata
 
+def __load_specialty_data(df,dcolumn,lcolumn):
+    """
+    Create Simple Training Data from List
+    ------------------------------------
 
+    Creates training data based on the keywords provided and the specialty associated with it.
+    """
+    #Set initial conditions
+    train_data = []
+    df[str(dcolumn)].astype(str)
+    df[str(lcolumn)].astype(str)
+    for row in df.itertuples():
+        pass
 
 def train_data(ldata):
     """
